147aeb983786d17f52eb45048ee6712d6b21830e
Robson Peacock <robson_peacock20@outlook.com>
2019-09-18 21:39:41 +0100

    Refactor objects in uploads controller

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -7,16 +7,10 @@ class UploadsController < ApplicationController
     obj = S3_BUCKET.objects[params[:file].original_filename]
 
     # Upload the file
-    obj.write(
-      file: params[:file],
-      acl: :bucket_owner_full_control
-    )
+    obj.write(file: params[:file], acl: :bucket_owner_full_control)
 
     # Create an object for the upload
-    @upload = Upload.new(
-    		url: obj.public_url,
-		    name: obj.key
-    	)
+    @upload = Upload.new(url: obj.public_url, name: obj.key)
 
     # Save the upload
     if @upload.save
